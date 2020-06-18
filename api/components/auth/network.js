cf5af705cd8da455d748787cdfb76eda96ede933
Razielini <razielco@gmail.com>
2020-06-18 08:10:14 -0600

    Create network.js

---
+++ api/components/auth/network.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const response = require('../../../network/response')
+const controller = require('./index')
+
+const router = express.Router()
+
+const login = (req, res) => {
+  controller.login(req.body.username, req.body.password)
+    .then(token => {
+      response.success(req, res, token, 200)
+    })
+    .catch(e => {
+      response.error(req, res, 'Información Invalida', 400)
+    })
+}
+
+router.post('/login', login)
+
+module.exports = router
