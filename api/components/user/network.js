ae5ecd4ebbd9a053e44b16c1fa506ac5c5fb262d
Razielini <razielco@gmail.com>
2020-06-19 22:35:51 -0600

    Update network.js

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -6,37 +6,40 @@ const controller = require('./index')
 
 const router = express.Router()
 
-const list = (req, res) => {
+const list = (req, res, next) => {
   controller.list()
     .then(lista => {
       response.success(req, res, lista, 200)
     })
-    .catch(err => {
-      response.error(req, res, err.message, 500)
-    })
+    .catch(next)
 }
 
-const get = (req, res) => {
+const get = (req, res, next) => {
   controller.get(req.params.id)
     .then(user => {
       response.success(req, res, user, 200)
     })
-    .catch(err => {
-      response.error(req, res, err.message, 500)
-    })
+    .catch(next)
 }
 
-const upsert = (req, res) => {
+const upsert = (req, res, next) => {
   controller.upsert(req.body)
     .then(user => {
       response.success(req, res, user, 201)
     })
-    .catch(err => {
-      response.error(req, res, err.message, 500)
+    .catch(next)
+}
+
+const follow = (req, res, next) => {
+  controller.follow(req.user.id, req.params.id)
+    .then(data => {
+      response.success(req, res, data, 201)
     })
+    .catch(next)
 }
 
 router.get('/', list)
+router.post('/follow/:id', secureAuth('follow'), follow)
 router.get('/:id', get)
 router.post('/', upsert)
 router.put('/', secureAuth('update'), upsert)
