f2558b7bc903375a666b2863aa9c147aae77a328
Razielini <razielco@gmail.com>
2020-06-19 22:35:38 -0600

    Update controller.js

--- api/components/auth/controller.js
+++ api/components/auth/controller.js
@@ -5,7 +5,7 @@ const TABLA = 'auth'
 module.exports = (injectedStore) => {
   let store = injectedStore
 
-  if (!store) store = require('../../../store/dummy')
+  if (!store) store = require('../../../store/mysql')
 
   const upsert = async (data) => {
     const authData = {
@@ -16,15 +16,20 @@ module.exports = (injectedStore) => {
 
     if (data.password) authData.password = await bcrypt.hash(data.password, 8)
 
+    console.log('Auth :: TABLA ::', TABLA)
+    console.log('Auth :: authData ::', authData)
     return store.upsert(TABLA, authData)
   }
 
   const login = async (username, password) => {
     const data = await store.query(TABLA, { username: username })
 
+    console.log('login :: password ::', password)
+    console.log('login :: data.password ::', data.password)
+
     return await bcrypt.compare(password, data.password)
     .then(areEquals => {
-      if (areEquals === true) return auth.sign(data)
+      if (areEquals === true) return auth.sign({ ...data})
     })
     .catch(err => {
       throw new Error('Información Invalida')
