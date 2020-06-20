b6065e2fa4b0c90affdac0fce86e18c950109341
Razielini <razielco@gmail.com>
2020-06-19 22:35:59 -0600

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,12 @@ module.exports = {
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'secret'
+  },
+  mysql: {
+    host: process.env.MYSQL_HOST || 'remotemysql.com',
+    port: process.env.MYSQL_PORT || 3306,
+    user: process.env.MYSQL_USER || 'FkABqWSNX5',
+    password: process.env.MYSQL_PASS || 's3faSlDk3o',
+    database: process.env.MYSQL_DB || 'FkABqWSNX5'
   }
 }
