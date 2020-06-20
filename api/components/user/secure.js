f5d775d9f4489b78f973abff28675176cd11156b
Razielini <razielco@gmail.com>
2020-06-19 22:35:53 -0600

    Update secure.js

--- api/components/user/secure.js
+++ api/components/user/secure.js
@@ -10,6 +10,10 @@ module.exports = checkAuth = (action) => {
         auth.check.own(req, owner)
         next()
         break
+      case 'follow': 
+        auth.check.logged(req)
+        next()
+        break
       default: next()
     }
   }
